8c00b26eb1a33889a67b58e89c6e5f593ba5210b
Damir Murat <damir.murat.git@gmail.com>
2014-12-17 20:10:46 +0100

    restoring 1.6 source compatibility

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,6 +3,7 @@ grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 grails.project.source.level = 1.6
+grails.project.target.level = 1.6
 
 grails.project.dependency.resolution = {
 
